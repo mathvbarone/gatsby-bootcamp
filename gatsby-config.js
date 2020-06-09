ce62539d4a4f4aad80771bfc3d59ea89dfd4bcc0
Matheus <matheus.barone@raisin.com>
2020-06-09 22:42:33 +0200

    Setup contenful api

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     author: "Matheus Barone",
   },
   plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
